ab39b845abd04e61d7933036e955b2f339843e66
gtsenkov <70806756+gtsenkov@users.noreply.github.com>
2020-10-30 09:02:12 +0200

    Add files via upload

---
+++ GenericsExercise/GenericSwapMethodInteger/Main.java
@@ -0,0 +1,32 @@
+package GenericsExercise.GenericSwapMethodInteger;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            int numbers = Integer.parseInt(scan.nextLine());
+            box.add(numbers);
+        }
+
+        String[] command = scan.nextLine().split(" ");
+
+        int firstIndex = Integer.parseInt(command[0]);
+        int secondIndex = Integer.parseInt(command[1]);
+
+        if (firstIndex < box.length() && secondIndex < box.length() && firstIndex >= 0 && secondIndex >= 0) {
+            box.swap(firstIndex, secondIndex);
+
+        } else {
+            throw new IndexOutOfBoundsException("Invalid indexes");
+        }
+
+
+
+        System.out.println(box);
+    }
+}
