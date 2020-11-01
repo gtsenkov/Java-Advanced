48936304d2dee5e6e933b19e69e4f676b9749866
gtsenkov <70806756+gtsenkov@users.noreply.github.com>
2020-11-01 08:50:56 +0200

    Add files via upload

---
+++ IteratorsAndComparatorsLab/ComparableBook/Book.java
@@ -0,0 +1,49 @@
+package IteratorsAndComparatorsLab.ComparableBook;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Book  implements Comparable<Book>{
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String title, int year, String... authors) {
+        this.setTitle(title);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(String... authors) {
+       this.authors = Arrays.asList(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+
+    @Override
+    public int compareTo(Book other) {
+       int compareResult =
+               this.getTitle().compareTo(other.getTitle());
+        if (compareResult == 0) {
+            compareResult = Integer.compare(this.year, other.year);
+        } return compareResult;
+    }
+}
---
+++ IteratorsAndComparatorsLab/ComparableBook/Main.java
@@ -0,0 +1,24 @@
+package IteratorsAndComparatorsLab.ComparableBook;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+
+//        List<Book> books = new ArrayList<>();
+//
+//        books.add(bookOne);
+//        books.add(bookTwo);
+//        books.add(bookThree);
+
+        if (bookOne.compareTo(bookTwo) > 0) {
+            System.out.println(String.format("%s is before %s", bookOne, bookTwo));
+        } else if (bookOne.compareTo(bookTwo) < 0) {
+            System.out.println(String.format("%s is before %s", bookTwo, bookOne));
+        } else {
+            System.out.println("Book are equal");
+        }
+    }
+
+}
